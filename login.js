ddc0e4eeb3503437bd2865cc94eec168ba4f778a
Mariasilva0218 <119074448+Mariasilva0218@users.noreply.github.com>
2022-12-21 18:04:34 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,36 +1,33 @@
-function login ()
+function login()
 {
-    const username = "maria";
-    const password= "senha123"; 
+    const login = "Lucas";
+    const password = "1234";
+   
+    let loginDig = document.getElementById("username").value;
+    let senhaDig = document.getElementById("password").value;
 
-    let usuario_informado = document.getElementById("username").value;
-    let senha_informada = document.getElementById("password").value;
-    
+    if(loginDig == login && senhaDig == password){ 
+        window.location.href = "index.html";
+        let continuarLogado = document.getElementById("remember-me");
 
-    if (usuario_informado == username && senha_informada == password){
-
-        let continuar_logado = document.getElementById("remember-me");     
-        alert (continuar_logado.checked);
-        if(continuar_logado){
+        if(continuarLogado){
             document.cookie = "logado=1";
 
-        }      
-
-        window.location.href = "index.html";        
-     } else {
-        alert ("Login ou Senha Inválidas");
-     } 
-    }
-
-    function registrar(){
-        let username = document.getElementById("username").value;
-        let email = document.getElementById("email").value;
-        let password = document.getElementById("password").value; 
-        let repeatpassword = document.getElementById("repeat-password").value;
-
-
-        if(password != repeatpassword){
-            alert("Você deve digitar duas senhas iguais");
         }
 
+    } else{
+        alert("Login inexistente");
     }
+} 
+
+function registrar() {
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let reptpassword = document.getElementById("rept-password").value;
+
+    if(password != reptpassword){
+        alert("Você deve digitar senhas iguais!");
+    }
+
+}
